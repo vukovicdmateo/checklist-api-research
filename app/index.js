750af7e10a4c12911640614e900f7894c18b5f74
Mateo Vuković <176288374+vukovicdmateo@users.noreply.github.com>
2024-07-30 12:48:42 +0200

    Implement a logging system

--- app/index.js
+++ app/index.js
@@ -1,10 +1,25 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 import { router as api } from './api/v1/index.js';
+import { HTTPlogger, logger } from './logger.js';
 
 export const app = express();
 
+// Parse JSON
 app.use(express.json());
+
+// Request ID
+app.use((req, res, next) => {
+  const id = uuidv4();
+  req.id = id;
+  res.setHeader('X-Request-Id', id);
+
+  next();
+});
+
+// Log HTTP Requests
+app.use(HTTPlogger);
 
 app.use('/api/v1', api);
 app.use('/api', api);
@@ -18,14 +33,22 @@ app.use((req, res, next) => {
 
 // Error handler
 app.use((err, req, res, next) => {
-  const { status = 500, message, error } = err;
-  res.status(status);
+  const { statusCode = 500, message, error } = err;
 
-  res.json({
-    error: {
-      status,
-      message,
-      error,
-    },
-  });
+  const data = {
+    message,
+    statusCode,
+    error,
+    traceId: req.id,
+  };
+
+  if (statusCode < 500) {
+    logger.warn(data);
+  } else {
+    logger.error(data);
+  }
+
+  res.status(statusCode);
+
+  res.json(data);
 });
